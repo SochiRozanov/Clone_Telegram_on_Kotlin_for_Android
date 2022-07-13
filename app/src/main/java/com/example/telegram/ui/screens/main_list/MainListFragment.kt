0c22eb43f1c562ecab39078deb7ebcb7df140f0b
polbuhanky <78269098+polbuhanky@users.noreply.github.com>
2022-07-14 02:45:41 +0300

    Запушил добавление сторизов

--- app/src/main/java/com/example/telegram/ui/screens/main_list/MainListFragment.kt
+++ app/src/main/java/com/example/telegram/ui/screens/main_list/MainListFragment.kt
@@ -1,10 +1,23 @@
 package com.example.telegram.ui.screens.main_list
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.telegram.R
 import com.example.telegram.database.*
 import com.example.telegram.models.CommonModel
+import com.example.telegram.ui.base.adapter.MainRowView
+import com.example.telegram.ui.base.adapter.viewholder.type.MainRowType
+import com.example.telegram.extension.replace
+import com.example.telegram.ui.screens.groups.GroupChatFragment
+import com.example.telegram.ui.screens.main_list.adapter.MainListAdapter
+import com.example.telegram.ui.screens.single_chat.SingleChatFragment
+import com.example.telegram.ui.screens.stories.StoriesFragment
+import com.example.telegram.ui.screens.stories.model.MediaType
+import com.example.telegram.ui.screens.stories.model.StoriesModel
 import com.example.telegram.utilits.*
 import kotlinx.android.synthetic.main.fragment_main_list.*
 
@@ -12,90 +25,79 @@ import kotlinx.android.synthetic.main.fragment_main_list.*
 
 class MainListFragment : Fragment(R.layout.fragment_main_list) {
 
-    private lateinit var mRecyclerView: RecyclerView
-    private lateinit var mAdapter: MainListAdapter
-    private val mRefMainList = REF_DATABASE_ROOT.child(NODE_MAIN_LIST).child(CURRENT_UID)
-    private val mRefUsers = REF_DATABASE_ROOT.child(NODE_USERS)
-    private val mRefMessages = REF_DATABASE_ROOT.child(NODE_MESSAGES).child(CURRENT_UID)
-    private var mListItems = listOf<CommonModel>()
+    private val viewModel: MainListViewModel by lazy {
+        MainListViewModel()
+    }
 
     override fun onResume() {
         super.onResume()
         APP_ACTIVITY.title = "Telegram"
         APP_ACTIVITY.mAppDrawer.enableDrawer()
         hideKeyboard()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         initRecyclerView()
+        initStoriesRecyclerView()
+        viewModel.mainListSetup()
+    }
+
+    private fun initStoriesRecyclerView() {
+        storiesRecyclerView.adapter = MainListAdapter().apply {
+
+            this.screenWidth = requireActivity().windowManager.defaultDisplay.width
+
+            this.interactionListener = object : MainListAdapter.InteractionListener {
+                override fun onBindViewHolder(holder: MainListAdapter.RowViewHolder, position: Int) {
+
+                    val mainRowView = holder.mainRow as MainRowView
+                    val rowType = MainRowType.fromValue("stories")
+
+                    mainRowView.setRecyclerView(rowType.viewType ?: return)
+
+                    when ("stories") {
+                        MainRowType.STORIES.value -> {
+                            mainRowView.setData(items)
+                            mainRowView.setOnItemClickListeners {
+                                replace(StoriesFragment.create(position, items))
+                            }
+                        }
+                    }
+                }
+                override fun getItemCount() = 1
+            }
+        }
     }
 
     private fun initRecyclerView() {
-        mRecyclerView = main_list_recycle_view
-        mAdapter = MainListAdapter()
+        main_list_recycle_view.adapter = MainListAdapter().apply {
 
-        // 1 запрос
-        mRefMainList.addListenerForSingleValueEvent(AppValueEventListener { dataSnapshot ->
-            mListItems = dataSnapshot.children.map { it.getCommonModel() }
-            mListItems.forEach { model ->
+            this.screenWidth = requireActivity().windowManager.defaultDisplay.width
 
-                when(model.type){
-                    TYPE_CHAT -> showChat(model)
-                    TYPE_GROUP -> showGroup(model)
+            this.interactionListener = object : MainListAdapter.InteractionListener {
+                override fun onBindViewHolder(holder: MainListAdapter.RowViewHolder, position: Int) {
+
+                    val mainRowView = holder.mainRow as MainRowView
+                    val rowType = MainRowType.fromValue("main_chats")
+
+                    mainRowView.setRecyclerView(rowType.viewType ?: return, LinearLayoutManager.VERTICAL)
+
+                    when ("main_chats") {
+                        MainRowType.MAIN_LIST.value -> {
+                            mainRowView.setLiveData(viewLifecycleOwner, viewModel.model)
+                            mainRowView.setOnItemClickListeners {
+                                val item = viewModel.model.value?.toList()?.get(position)
+                                when (item?.type) {
+                                    TYPE_CHAT -> replace(SingleChatFragment(item))
+                                    TYPE_GROUP -> replace(GroupChatFragment(item))
+                                }
+                            }
+                        }
+                    }
                 }
-
+                override fun getItemCount() = 1
             }
-        })
-
-        mRecyclerView.adapter = mAdapter
-    }
-
-    private fun showGroup(model: CommonModel) {
-        // 2 запрос
-        REF_DATABASE_ROOT.child(NODE_GROUPS).child(model.id)
-            .addListenerForSingleValueEvent(AppValueEventListener { dataSnapshot1 ->
-                val newModel = dataSnapshot1.getCommonModel()
-
-                // 3 запрос
-                REF_DATABASE_ROOT.child(NODE_GROUPS).child(model.id).child(NODE_MESSAGES)
-                    .limitToLast(1)
-                    .addListenerForSingleValueEvent(AppValueEventListener { dataSnapshot2 ->
-                        val tempList = dataSnapshot2.children.map { it.getCommonModel() }
-
-                        if (tempList.isEmpty()){
-                            newModel.lastMessage = "Чат очищен"
-                        } else {
-                            newModel.lastMessage = tempList[0].text
-                        }
-                        newModel.type = TYPE_GROUP
-                        mAdapter.updateListItems(newModel)
-                    })
-            })
-
-    }
-
-    private fun showChat(model: CommonModel) {
-        // 2 запрос
-        mRefUsers.child(model.id)
-            .addListenerForSingleValueEvent(AppValueEventListener { dataSnapshot1 ->
-                val newModel = dataSnapshot1.getCommonModel()
-
-                // 3 запрос
-                mRefMessages.child(model.id).limitToLast(1)
-                    .addListenerForSingleValueEvent(AppValueEventListener { dataSnapshot2 ->
-                        val tempList = dataSnapshot2.children.map { it.getCommonModel() }
-
-                        if (tempList.isEmpty()){
-                            newModel.lastMessage = "Чат очищен"
-                        } else {
-                            newModel.lastMessage = tempList[0].text
-                        }
-
-
-                        if (newModel.fullname.isEmpty()) {
-                            newModel.fullname = newModel.phone
-                        }
-
-                        newModel.type = TYPE_CHAT
-                        mAdapter.updateListItems(newModel)
-                    })
-            })
+        }
     }
 }
